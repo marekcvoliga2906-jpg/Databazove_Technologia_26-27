41dc9172b96661b474f1fe6d34ea099f461202d2
Marek Cvoliga <marekcvoliga2906@gmail.com>
2026-09-22 11:54:13 +0200

    Pridanie databazy

---
+++ uloha1.sql
@@ -0,0 +1,34 @@
+create database superstore
+USE superstore;
+
+CREATE TABLE customers (
+    customer_id VARCHAR(20) PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    segment VARCHAR(50) NOT NULL,
+    country VARCHAR(50)  NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(20) PRIMARY KEY,
+    category VARCHAR(50) NOT NULL,
+    sub_category VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100)  NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(20) PRIMARY KEY,
+    customer_id VARCHAR(20)  NOT NULL,
+    product_id VARCHAR(50) NOT NULL,
+    order_date DATE NOT NULL,
+    ship_date DATE NOT NULL,
+    sales NUMERIC(10,2) NOT NULL,
+    quantity INTEGER NOT NULL,
+    discount NUMERIC(10,2) NOT NULL,
+    profit NUMERIC(10,2) NOT NULL
+
+    FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES
+products(product_id)    
+);
